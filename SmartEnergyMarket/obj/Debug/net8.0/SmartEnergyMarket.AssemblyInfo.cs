68d4c14de777b8d6d2f10dde4ad7a5c4fe982d21
Safni-hsn <2020e136@eng,jfn.ac.lk>
2025-06-02 15:28:15 +0530

    neww

--- SmartEnergyMarket/obj/Debug/net8.0/SmartEnergyMarket.AssemblyInfo.cs
+++ SmartEnergyMarket/obj/Debug/net8.0/SmartEnergyMarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3d423c43b0665ab1782d94b53a9a07a31aa371e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
