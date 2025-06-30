053a5814abdd5097d75c7617541dd365343772cd
Safni-hsn <2020e136@eng,jfn.ac.lk>
2025-06-30 11:42:36 +0530

    Database Hosted

--- SmartEnergyMarket/obj/Debug/net8.0/SmartEnergyMarket.AssemblyInfo.cs
+++ SmartEnergyMarket/obj/Debug/net8.0/SmartEnergyMarket.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f00a141356d7d45416f3cc0151de30248e23742")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b997bc9c2cde2fac3f2e05fb7ffecbf5b0955f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartEnergyMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
